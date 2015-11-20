8ef4b6ab2034da142fcb18d4831b063bd46822c7
Jonathan Mezach <jonathanm@infosupport.com>
2015-11-20 08:44:42 +0100

    Set up a test harness for TFS build tasks

--- integration/tfs-build-task-tests/PublishFeaturesToAugurk/PublishFeaturesToAugurk.Tests.ps1
+++ integration/tfs-build-task-tests/PublishFeaturesToAugurk/PublishFeaturesToAugurk.Tests.ps1
@@ -1,39 +1,39 @@
-$sut = "$PSScriptRoot\..\..\tfs-build-task\PublishFeaturesToAugurk\PublishFeaturesToAugurk.ps1"
-
-# Dummy function definitions so that we can Mock them
-Function Find-Files { }
-Function Get-ServiceEndpoint { }
-Function Invoke-Tool { }
+Import-Module "$PSScriptRoot\..\BuildTaskTestHelper.psm1"
 
 Describe "Publishes Features To Augurk" {
-	Mock Import-Module
-	Mock Get-ServiceEndpoint { return "UrlToAugurkService" }
-	
-	Context "When Group Name Is Provided" {
-		Mock Find-Files { return [PSCustomObject]@{FullName = "SomeInteresting.feature"} }
-		Mock Invoke-Tool -Verifiable -ParameterFilter { $Arguments -like "*--groupName=SomeGroupName*" }
-		$augurk = New-Item TestDrive:\augurk.exe -Type File
-			
-		& $sut -features "**\*.features" -connectedServiceName "SomeAugurkService" -productName "SomeProductName" `
-			   -version "SomeVersion" -useFolderStructure "false" -groupName "SomeGroupName" -language "en-US" `
-			   -augurkLocation $augurk.FullName -treatWarningsAsErrors "false"
-			   
-		It "Calls augurk.exe with the provided group name" {
-			Assert-VerifiableMocks
-		}
-	}
-	
-	Context "When Folder Structure is used" {
-		Mock Find-Files { return [PSCustomObject]@{FullName = "SomeInteresting.feature"; Directory = "SomeParentFolder"} }
-		Mock Invoke-Tool -Verifiable -ParameterFilter { $Arguments -like "*--groupName=SomeParentFolder*" }
-		$augurk = New-Item TestDrive:\augurk.exe -Type File
+	InModuleScope BuildTaskTestHelper {
+		$sut = "$PSScriptRoot\..\..\tfs-build-task\PublishFeaturesToAugurk\task.json"
 		
-		& $sut -features "**\*.features" -connectedServiceName "SomeAugurkService" -productName "SomeProductName" `
-			   -version "SomeVersion" -useFolderStructure "true" -language "en-US" `
-			   -augurkLocation $augurk.FullName -treatWarningsAsErrors "false"
-			   
-		It "Calls augurk.exe with the provided group name" {
-			Assert-VerifiableMocks
+		# Dummy function definitions so that we can Mock them
+		Function Find-Files { }
+		Function Get-ServiceEndpoint { }
+		Function Invoke-Tool { }
+		
+		Mock Import-Module
+		Mock Get-ServiceEndpoint { return "UrlToAugurkService" }
+		
+		Context "When Group Name Is Provided" {
+			Mock Find-Files { return [PSCustomObject]@{FullName = "SomeInteresting.feature"} }
+			Mock Invoke-Tool -Verifiable -ParameterFilter { $Arguments -like "*--groupName=SomeGroupName*" }
+			$augurk = New-Item TestDrive:\augurk.exe -Type File
+			
+			Invoke-BuildTask -TaskDefinitionFile $sut -- -connectedServiceName "SomeAugurkService" -productName "SomeProduct" -version "SomeVersion" -groupName "SomeGroupName" 
+				
+			It "Calls augurk.exe with the provided group name" {
+				Assert-VerifiableMocks
+			}
+		}
+		
+		Context "When Folder Structure is used" {
+			Mock Find-Files { return [PSCustomObject]@{FullName = "SomeInteresting.feature"; Directory = "SomeParentFolder"} }
+			Mock Invoke-Tool -Verifiable -ParameterFilter { $Arguments -like "*--groupName=SomeParentFolder*" }
+			$augurk = New-Item TestDrive:\augurk.exe -Type File
+			
+			Invoke-BuildTask -TaskDefinitionFile $sut -- -connectedServiceName "SomeAugurkService" -productName "SomeProduct" -version "SomeVersion" -useFolderStructure "True"
+				
+			It "Calls augurk.exe with the provided group name" {
+				Assert-VerifiableMocks
+			}
 		}
 	}
 }
